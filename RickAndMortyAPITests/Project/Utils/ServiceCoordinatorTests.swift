1d6cc4c8bd0b2c5bdd3f80a13a0472b0a492e75e
maxeziowolf <maxeziowolf@outlook.com>
2023-01-20 15:55:49 -0600

    Se agregraron pruebas unitarias del ServiceCoordinator

--- RickAndMortyAPITests/Project/Utils/ServiceCoordinatorTests.swift
+++ RickAndMortyAPITests/Project/Utils/ServiceCoordinatorTests.swift
@@ -10,39 +10,176 @@ import XCTest
 
 final class ServiceCoordinatorTests: XCTestCase {
     
-    func testBuildCorrectURLWithoutParams() throws {
+    override class func setUp() {
+        // Se ejecuta antes de iniciar cada test
+    }
+    
+    override class func tearDown() {
+        // Se ejecuta al finalizar cada test
+    }
+    
+    func testBuildCorrectURLWithoutParams(){
+        // 1.-Given
+        let urlCorrect = URL(string: "https://rickandmortyapi.com/api/character")
+        let urlTest = "https://rickandmortyapi.com/api/character"
         
-        let urlCorrect = URL(string: "www.example.com/help")
-        
-        let urlTest = "www.example.com/help"
-        
+        // 2.-When
         let urlGet = ServiceCoordinator.getURL(url: urlTest, parameters: nil)
         
-        print("url correcta: \(String(describing: urlCorrect))")
-        print("url obtenida: \(String(describing: urlGet))")
-        
+        // 3.-Then
         XCTAssertEqual(urlCorrect, urlGet, "No se obtuvo correctamente la URL sin parametros")
         
     }
 
 
-    func testBuildCorrectURLWithParams() throws{
+    func testBuildCorrectURLWithParams(){
+        // 1.-Given
+        let urlCorrect = URL(string: "https://rickandmortyapi.com/api/character/?page=19")
+        let urlTest = "https://rickandmortyapi.com/api/character/"
+        let parameters: [String : Any] = ["page":19]
         
-        let urlCorrect = URL(string: "www.example.com/help?id=1&count=2")
-        
-        let urlTest = "www.example.com/help"
-        
-        let parameters: [String : Any] = ["id":1, "count":"2"]
-        
+        // 2.-When
         let urlGet = ServiceCoordinator.getURL(url: urlTest, parameters: parameters)
         
-        print("url correcta: \(String(describing: urlCorrect))")
-        print("url obtenida: \(String(describing: urlGet))")
-        
+        // 3.-Then
         XCTAssertEqual(urlCorrect, urlGet, "No se obtuvo correctamente la URL con parametros")
         
     }
     
+    func testPerformanceBuildURLWithoutParams() {
+        
+        self.measure {
+            // 1.-Given
+            let urlCorrect = URL(string: "https://rickandmortyapi.com/api/character/character/character/longer")
+            let urlTest = "https://rickandmortyapi.com/api/character/character/character/longer"
+            
+            // 2.-When
+            let urlGet = ServiceCoordinator.getURL(url: urlTest, parameters: nil)
+            
+            // 3.-Then
+            XCTAssertEqual(urlCorrect, urlGet, "No se obtuvo correctamente la URL sin parametros")
+        }
+        
+    }
+    
+    func testPerformanceBuildURLWithParams() {
+        
+        self.measure {
+            // 1.-Given
+            let urlCorrect = URL(string: "https://rickandmortyapi.com/api/character/?page=19")
+            let urlTest = "https://rickandmortyapi.com/api/character/"
+            let parameters: [String : Any] = ["page":19]
+            
+            // 2.-When
+            let urlGet = ServiceCoordinator.getURL(url: urlTest, parameters: parameters)
+            
+            // 3.-Then
+            XCTAssertEqual(urlCorrect, urlGet, "No se obtuvo correctamente la URL con parametros")
+        }
+        
+    }
+    
+    func testSendResquestCorrect(){
+        
+        // 1.-Given
+        var correctCompletion = false
+        let url = "https://rickandmortyapi.com/api/character"
+        
+        // 2.-When
+        ServiceCoordinator.sendRequest(url: url) { (response: ServiceStatus<ResponseAPI>) in
+            switch response{
+            case .success(data: _):
+                // 3.-Then
+                correctCompletion = true
+            case .failed(error: _):
+                // 3.-Then
+                correctCompletion = false
+            case .unowned(error: _):
+                // 3.-Then
+                correctCompletion = false
+            }
+            XCTAssert(correctCompletion, "No se completo correctamente la peticion")
+        }
+        
+    }
+    
+    func testSendResquestCorrectWithParams(){
+        
+        // 1.-Given
+        var correctCompletion = false
+        let url = "https://rickandmortyapi.com/api/character/"
+        let parameters: [String : Any] = ["page":19]
+        
+        // 2.-When
+        ServiceCoordinator.sendRequest(url: url, parameters: parameters) { (response: ServiceStatus<ResponseAPI>) in
+            switch response{
+            case .success(data: _):
+                // 3.-Then
+                correctCompletion = true
+            case .failed(error: _):
+                // 3.-Then
+                correctCompletion = false
+            case .unowned(error: _):
+                // 3.-Then
+                correctCompletion = false
+            }
+            XCTAssert(correctCompletion, "No se completo correctamente la peticion")
+        }
+        
+    }
+    
+    func testPerformanceSendResquestCorrect(){
+        
+        self.measure {
+            // 1.-Given
+            var correctCompletion = false
+            let url = "https://rickandmortyapi.com/api/character"
+            
+            // 2.-When
+            ServiceCoordinator.sendRequest(url: url) { (response: ServiceStatus<ResponseAPI>) in
+                switch response{
+                case .success(data: _):
+                    // 3.-Then
+                    correctCompletion = true
+                case .failed(error: _):
+                    // 3.-Then
+                    correctCompletion = false
+                case .unowned(error: _):
+                    // 3.-Then
+                    correctCompletion = false
+                }
+                XCTAssert(correctCompletion, "No se completo correctamente la peticion")
+            }
+        }
+        
+    }
+    
+    func testPerformanceSendResquestCorrectWithParams(){
+        
+        self.measure {
+            // 1.-Given
+            var correctCompletion = false
+            let url = "https://rickandmortyapi.com/api/character/"
+            let parameters: [String : Any] = ["page":19]
+            
+            // 2.-When
+            ServiceCoordinator.sendRequest(url: url, parameters: parameters) { (response: ServiceStatus<ResponseAPI>) in
+                switch response{
+                case .success(data: _):
+                    // 3.-Then
+                    correctCompletion = true
+                case .failed(error: _):
+                    // 3.-Then
+                    correctCompletion = false
+                case .unowned(error: _):
+                    // 3.-Then
+                    correctCompletion = false
+                }
+                XCTAssert(correctCompletion, "No se completo correctamente la peticion")
+            }
+        }
+        
+    }
 
     
 
